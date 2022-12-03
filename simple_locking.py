b7737ef2601c5040c8b231c3321364667bfaa6d9
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2022-12-03 22:31:22 +0700

    Update simple_locking.py

--- simple_locking.py
+++ simple_locking.py
@@ -1,7 +1,6 @@
 class SimpleLock:
     def __init__(self, jadwal : list):
         self.jadwal = jadwal
-        self.operationList = {'R' : 'Read', 'W' : 'Write', 'C' : 'Commit'}
         self.dataItem = self.getAllItem()
         self.finalJadwal = self.getNewSchedule()
 
@@ -13,23 +12,32 @@ class SimpleLock:
         return dataItem
 
     def getNewSchedule(self):
-        jadwal = self.jadwal
+        jadwal = self.jadwal.copy()
         finalSchedule = []
         queue = []
         i = 0
         while i != len(jadwal):
             if jadwal[i][0] == "C":
-                finalSchedule.append(jadwal[i])
-                self.releaseLock(jadwal[i][1])
-                if len(queue) != 0:
-                    newJadwal, newQueue = self.checkAvailable(jadwal, queue, i)
-                    jadwal = newJadwal
-                    queue = newQueue
+                if self.checkQueue(jadwal[i][1], queue):
+                    queue.append(jadwal[i])
+                    jadwal.remove(jadwal[i])
+                    i -= 1
+                else:
+                    finalSchedule.append(jadwal[i])
+                    self.releaseLock(jadwal[i][1])
+                    if len(queue) != 0:
+                        newJadwal, newQueue = self.checkAvailable(jadwal, queue, i)
+                        jadwal = newJadwal
+                        queue = newQueue
             else:
-                if self.dataItem.get(jadwal[i][3]) == "Free":
+                if self.checkQueue(jadwal[i][1], queue):
+                    queue.append(jadwal[i])
+                    jadwal.remove(jadwal[i])
+                    i -= 1
+                elif self.dataItem.get(jadwal[i][3]) == "Free":
                     finalSchedule.append("XL" + jadwal[i][1:])
                     finalSchedule.append(jadwal[i])
-                    self.dataItem[self.jadwal[i][3]] = self.jadwal[i][1]
+                    self.dataItem[jadwal[i][3]] = jadwal[i][1]
                 elif self.dataItem.get(jadwal[i][3]) == jadwal[i][1]:
                     finalSchedule.append(jadwal[i])
                 else:
@@ -37,8 +45,23 @@ class SimpleLock:
                     jadwal.remove(jadwal[i])
                     i -= 1
             i += 1
-        print(finalSchedule)
+        
+        if len(queue) != 0:
+            finalSchedule.append("deadlock")
         return finalSchedule
+
+    def checkQueue(self, T, queue):
+        for i in range(len(queue)):
+            if queue[i][1] == T:
+                return True
+        return False
+
+    def unlock(self, final, T):
+        tempFinal = final.copy()
+        for item in final:
+            if item[0] == 'X' and item[2] == T:
+                tempFinal.append("U" + item[1:])
+        return tempFinal
 
     def releaseLock(self, T):
         for item in self.dataItem.keys():
@@ -47,12 +70,43 @@ class SimpleLock:
 
     def checkAvailable(self, jadwal : list, queue : list, index):
         index += 1
-        for item in queue:
-            if self.dataItem.get(item[3]) == "Free":
-                jadwal.insert(index, item)
-                queue.remove(item)
-                index += 1
+        i = 0
+        while i != len(queue):
+            if queue[i][0] == "C":
+                if not self.checkCommit(queue, queue[i][1]):
+                    jadwal.insert(index, queue[i])
+                    queue.remove(queue[i])
+                    index += 1
+                    i -= 1
+                i += 1 
+            else:
+                if self.dataItem.get(queue[i][3]) == "Free" or self.dataItem.get(jadwal[i][3]) == queue[i][1]:
+                    jadwal.insert(index, queue[i])
+                    queue.remove(queue[i])
+                    index += 1
+                    i -= 1
+                i += 1
         return jadwal, queue
 
+    def checkCommit(self, queue : list, T):
+        for item in queue:
+            if item[0] != "C":
+                if item[1] == T:
+                    return True
+        return False
 
-test = SimpleLock(["R1(X)", "R2(X)", "R3(X)", "C1", "C2", "C3"])
+    def printFinal(self):
+        print("Initial Schedule = ", self.jadwal)
+        print("Final Schedule = ", self.finalJadwal)
+
+#test = SimpleLock(["R1(X)", "R2(X)", "C1", "R2(Y)", "C2"])
+#test = SimpleLock(["R1(X)", "R2(Y)", "C1", "C2"])
+#test = SimpleLock(["R1(A)", "W1(A)", "R2(A)", "W2(A)", "R1(B)", "W1(B)", "R2(B)", "W2(B)", "C1", "C2"])
+
+
+#test = SimpleLock(["R1(X)", "R2(Y)", "R1(Y)", "W2(Y)", "W1(X)", "C1", "C2"])
+#test = SimpleLock(["R1(X)", "R2(Y)", "R1(Y)", "R2(X)", "C1", "C2"])
+#test = SimpleLock(["R1(X)", "W2(X)", "W2(Y)", "W3(Y)", "W1(X)", "C1", "C2", "C3"])
+#test = SimpleLock(["R1(X)", "R3(Y)", "R3(X)", "W3(Y)", "R2(Y)", "R1(Y)", "W3(X)", "R2(X)", "C1", "C2", "C3"])
+test = SimpleLock(["R1(X)", "R2(Z)", "R3(X)", "R3(Y)", "W1(X)", "C1", "W3(Y)", "C3", "R2(Y)", "W2(Z)", "W2(Y)", "C2"])
+test.printFinal()
