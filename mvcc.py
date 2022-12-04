dac2337f2922591f6586722b27df47f386c0918c
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-12-04 21:26:32 +0700

    MVCC Finish

--- mvcc.py
+++ mvcc.py
@@ -28,7 +28,7 @@ class MVCC:
         while (not(read_time in self.eachTransaction)):
             read_time = self.changedTimestamp[read_time]
         for i in range(len(self.eachTimestamp[data])):
-            if (read_time > self.eachTimestamp[data][i][0] and read_time < self.eachTimestamp[data][i][1]):
+            if (read_time > self.eachTimestamp[data][i][0]):
                 idx = i
         if (self.eachTimestamp[data][idx][0] < read_time):
             self.eachTimestamp[data][idx][0] = read_time
@@ -45,7 +45,8 @@ class MVCC:
                 is_abort = True
                 version = self.eachTimestamp[data][i][2]
         if (not(is_abort)):
-            self.eachTimestamp[data].append([write_time, write_time, first_write_time])
+            if (not([write_time, write_time, first_write_time] in self.eachTimestamp[data])):
+                self.eachTimestamp[data].append([write_time, write_time, first_write_time])
             print("TS(" + data + str(first_write_time) + ") = (" + str(write_time) + ", " + str(write_time) + ")")
         else:
             self.maxTimestamp += 1
@@ -71,5 +72,7 @@ class MVCC:
             elif (self.queue[i][0][0] == "W"):
                 print(self.queue[i][0], "->     ", end="")
                 self.write(self.queue[i][0][3], int(self.queue[i][0][1]), self.queue[i][1])
+        print(self.eachTimestamp)
 
-schedule = MVCC(["R1(A)", "R2(A)", "R3(B)", "R1(B)", "W3(C)", "W2(C)", "R1(C)", "C1", "R2(D)", "W3(B)", "C3", "W2(D)", "C2"])
+# schedule = MVCC(["R1(A)", "R2(A)", "R3(B)", "R1(B)", "W3(C)", "W3(C)", "R1(C)", "C1", "R2(D)", "W3(B)", "C3", "W2(D)", "C2"])
+schedule = MVCC(["R5(X)", "R2(Y)", "R1(Y)", "W3(Y)", "W3(Z)", "R5(Z)", "R2(Z)", "R1(X)", "R4(W)", "W3(W)", "W5(Y)", "W5(Z)"])
